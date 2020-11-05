7fda17baccbca3a24dbf5abb936bd29d0d6e7030
Magda <otherselkie@gmail.com>
2020-11-05 09:26:38 +0100

    Character's stop states added

--- Assets/CharacterController2D.cs
+++ Assets/CharacterController2D.cs
@@ -8,7 +8,9 @@ public class CharacterController2D : MonoBehaviour
     Rigidbody2D rigidbody2d;
     [SerializeField] float speed = 2f;
     Vector2 motionVector;
+    public Vector2 lastMotionVector;
     Animator animator;
+    public bool moving;
     void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
@@ -16,12 +18,28 @@ public class CharacterController2D : MonoBehaviour
     }
     private void Update()
     {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
         motionVector = new Vector2(
-            Input.GetAxisRaw("Horizontal"), 
-            Input.GetAxisRaw("Vertical")
+            horizontal,
+            vertical
             );
-        animator.SetFloat("horizontal", Input.GetAxisRaw("Horizontal"));
-        animator.SetFloat("vertical", Input.GetAxisRaw("Vertical"));
+        animator.SetFloat("horizontal", horizontal);
+        animator.SetFloat("vertical", vertical);
+
+
+        moving = horizontal != 0 || vertical != 0;
+        animator.SetBool("moving", moving);
+
+        if (horizontal !=0 || vertical !=0)
+        {
+            lastMotionVector = new Vector2(
+                horizontal,
+                vertical
+                ).normalized;
+            animator.SetFloat("lastHorizontal", horizontal);
+            animator.SetFloat("lastVertical", vertical);
+        }
     }
     void FixedUpdate()
     {
