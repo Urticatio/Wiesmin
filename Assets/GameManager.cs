09c042d4cd0c0c6ee8cc51887c3ca8580eb8b833
Maciej <kordeg97@gmail.com>
2020-11-23 22:21:18 +0100

    Dragging works, dropping not

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -13,4 +13,5 @@ public class GameManager : MonoBehaviour
     public GameObject player;
     public ItemContainer inventoryContainer;
     public DragAndDropController dragDropController;
+    public ItemSpawnManager itemSpawnManager;
 }
---
+++ Assets/ItemSpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Item spawn manager")]
+public class ItemSpawnManager : ScriptableObject
+{
+    public static ItemSpawnManager instance;//statyczne
+
+    private void Start()
+    {
+        instance = this;
+    }
+
+    [SerializeField] GameObject pickUpItemPrefab;
+    public void SpawnItem(Vector3 position, Item item, int count)
+    {
+        GameObject o = Instantiate(pickUpItemPrefab, position, Quaternion.identity);
+        o.GetComponent<PickUpItem>().Set(item, count);
+    }
+}
