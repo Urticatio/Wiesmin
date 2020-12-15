0c3230e222d22865339a390296e736d6ad18a83d
Maciej <kordeg97@gmail.com>
2020-12-15 11:35:36 +0100

    Toolbar fix

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -15,4 +15,5 @@ public class GameManager : MonoBehaviour
     public ItemContainer inventoryContainer;
     public DragAndDropController dragAndDropController;
     public ItemSpawnManager itemSpawnManager;
+    public GameObject toolbarPanel;
 }
--- Assets/Scripts/ToolbarController.cs
+++ Assets/Scripts/ToolbarController.cs
@@ -11,9 +11,9 @@ public class ToolbarController : MonoBehaviour
     private void Update()
     {
         float delta = Input.mouseScrollDelta.y;
-        if(delta != 0)
+        if(delta != 0) //gdy scrollujemy
         {
-            if(delta > 0)
+            if(delta < 0)
             {
                 selectedTool += 1;
                 selectedTool = (selectedTool >= toolbarSize ? 0 : selectedTool);
@@ -21,7 +21,7 @@ public class ToolbarController : MonoBehaviour
             else
             {
                 selectedTool -= 1;
-                selectedTool = (selectedTool <= 0 ? toolbarSize -1 : selectedTool);
+                selectedTool = (selectedTool < 0 ? toolbarSize-1 : selectedTool);
             }
             onChange?.Invoke(selectedTool);
         }
